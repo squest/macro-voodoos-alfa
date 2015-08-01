5824ae5a005a30ef562ab7b318f4ae7175cafb85
squest <sabdaps@gmail.com>
2015-08-02 01:59:25 +0700

    for-var macro

--- src/macros/core.clj
+++ src/macros/core.clj
@@ -1,6 +1,20 @@
 (ns macros.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro for-var
+  [reducer-fn lst]
+  (let [[l ls res r l1] (repeatedly 5 gensym)]
+    `(loop [[~l & ~ls] (rest ~lst) ~res (map vector (first ~lst))]
+       (if ~l
+         (recur ~ls (for [~r ~res ~l1 ~l] (conj ~r ~l1)))
+         (map ~reducer-fn ~res)))))
+
+(defn salmons
+  [& lst]
+  (for-var (partial into #{}) lst))
+
+(defmacro for-var1
+  [reducer-fn lst]
+  (let [comps (vec (repeatedly 10 gensym))
+        [ctr binds] (repeatedly 2 gensym)]
+    `(let [~binds (vec (mapcat vector ~comps ~lst))]
+       (for ~binds (apply ~reducer-fn (take ~ctr comps))))))
